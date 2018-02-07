2017b19098bf2e98be865171db9f6478f742ffdd
kyriosdata <fabio@inf.ufg.br>
2018-02-07 14:05:52 -0200

    segundo exemplo

---
+++ segundo/README.md
@@ -0,0 +1,5 @@
+# Segundo aplicativo em JavaScript
+Aplicativo que exibe informação na saída padrão, 
+inclusive o resultado da chamada da função **quadrado**.
+
+Veja aqui como executar este aplicativo. 
---
+++ segundo/quadrado.js
@@ -0,0 +1,9 @@
+console.log("quadrado (versão 1.0.0)");
+console.log("(meu segundo aplicativo em JavaScript)");
+
+function quadrado(x) {
+  return x * x;
+}
+
+console.log("");
+console.log("O quadrado de 2 é " + quadrado(2));
