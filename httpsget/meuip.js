c24a47177431834b1397dab8a91f24e9b963f136
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-02-12 18:41:25 -0200

    call usando promise

--- httpsget/meuip.js
+++ httpsget/meuip.js
@@ -1,20 +1,27 @@
 const https = require('https');
-const log = x => console.log(x);
 const URL = "https://httpbin.org/ip";
-const erro = err => log(err);
-const ip = dados => log(JSON.parse(dados).origin);
+const log = console.log;
 
-function handler(resp, faz) {
-  let data = '';
- 
-  resp.on("data", chunk => data += chunk); 
-  resp.on("end", () => ip(data));
+function parametro(funcao) {
+  return function handler(resp) {
+    let data = '';
+
+    resp.on("data", chunk => data += chunk);
+    resp.on("end", () => funcao(data));
+  }
 }
 
-https.get(URL, handler).on("error", erro);
-log("vou aguardar pelo retorno...");
-
-function get(url) {
-  return new Promise(function(resolve, reject) { 
-  });
+function corpo(resolve, reject) {
+  function ok(dados) {
+    resolve(JSON.parse(dados).origin);
+  }
+  
+  function falha(erro) {
+    reject("Houve um erro...");
+  }
+  
+  log("Qual meu IP? (acessando serviço remoto...)");
+  https.get(URL, parametro(ok)).on("error", falha);
 }
+
+new Promise(corpo).then(log, log);
