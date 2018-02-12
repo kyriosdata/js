7478c61f82ca806f5e298e1c0d27a8905c6a0731
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-02-12 17:22:03 -0200

    melhorias

--- httpsget/meuip.js
+++ httpsget/meuip.js
@@ -1,14 +1,20 @@
 const https = require('https');
 const log = x => console.log(x);
 const URL = "https://httpbin.org/ip";
+const erro = err => log(err);
+const ip = dados => log(JSON.parse(dados).origin);
 
-function handler(resp) {
+function handler(resp, faz) {
   let data = '';
  
   resp.on("data", chunk => data += chunk); 
-  resp.on("end", () => log(JSON.parse(data).origin));
+  resp.on("end", () => ip(data));
 }
 
-const erro = err => log(err);
-
 https.get(URL, handler).on("error", erro);
+log("vou aguardar pelo retorno...");
+
+function get(url) {
+  return new Promise(function(resolve, reject) { 
+  });
+}
