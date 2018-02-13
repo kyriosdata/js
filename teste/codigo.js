036e9b49c2706a2c4f4aba588fa75d3eba8bc59e
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-02-13 19:30:18 -0200

    texto

--- teste/codigo.js
+++ teste/codigo.js
@@ -1,3 +1,8 @@
+// Função que retorna a soma dos argumentos fornecidos.
+// Espera-se que esta função seja usada não apenas neste
+// arquivo, mas também além. Em consequência, exportamos
+// a função (exports) ao criar um módulo. 
+
 exports.soma = function (x, y) {
   return x + y;
 };
