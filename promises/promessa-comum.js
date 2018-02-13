659d66457031a05d83928aea7a8cae95799f60f2
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-02-13 08:59:14 -0200

    exemplos

--- promises/promessa-comum.js
+++ promises/promessa-comum.js
@@ -1,27 +1,63 @@
 const https = require('https');
 
 function httpsGet(url) {
-  const log = console.log;
 
-  function parametro(funcao) {
-    return function handler(resp) {
+  function executor(resolve, reject) {
+
+    function handler(resp) {
       let data = '';
 
       resp.on("data", chunk => data += chunk);
-      resp.on("end", () => funcao(data));
+      resp.on("end", () => resolve(data));
     }
-  }
 
-  function executor(resolve, reject) {
-    let ok = d => resolve(JSON.parse(d).now.epoch);
-    let falha = e => reject("Houve um erro...");
+    let falha = e => reject(e);
 
-    log("Qual meu IP? (acessando serviço remoto...)");
-    https.get(url, parametro(ok)).on("error", falha);
+    https.get(url, handler).on("error", falha);
   }
 
   return new Promise(executor);
 }
 
+// Vamos obter a hora corrente?
+// Em geral isso é feito em poucas linhas de código
+// em JavaScript. Abaixo segue uma versão "longa"
+// que facilita a compreensão do que está ocorrendo.
+
+// Algoritmo
+// 1. Requisitar a hora corrente
+// 2. Realizar o parsing dos dados
+// 3. Extrair a informação relevante
+// 4. Montar uma data correspondente
+// 5. Exibir a data.
+
+function horaCorrente(url) {
+  return httpsGet(url);
+}
+
+function parsing(dados) {
+  return JSON.parse(dados);
+}
+
+function extrair(dados) {
+  return dados.now.epoch;
+}
+
+function montarData(epoca) {
+  var utcSeconds = epoca;
+  var data = new Date(0);
+  data.setUTCSeconds(utcSeconds);
+  
+  return data;
+}
+
+function exibir(data) {
+  console.log(data);
+}
+
 const URL = "https://now.httpbin.org";
-httpsGet(URL).then(console.log, console.log);
+horaCorrente(URL)
+  .then(parsing, console.log)
+  .then(extrair, console.log)
+  .then(montarData, console.log)
+  .then(exibir, console.log);
