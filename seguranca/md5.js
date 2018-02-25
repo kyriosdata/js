f2b5b588d7a717e4258b234d34182345587085ed
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-02-25 15:42:34 -0300

    md5

---
+++ seguranca/md5.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const seguranca = require("crypto");
+
+// Arquivo de entrada (fornecido via linha de comandos)
+const arquivo = process.argv[2];
+
+const hash = seguranca.createHash("md5");
+const entrada = fs.createReadStream(arquivo);
+
+entrada.on("readable", () => {
+    const data = entrada.read();
+    if (data) {
+        hash.update(data);
+    }
+});
+
+entrada.on("end", () => {
+    let md5 = hash.digest("hex");
+    console.log(`${md5} ${arquivo}`);
+});
