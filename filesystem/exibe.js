9ca5c5447936b9fc4f38b103124546ef4cd835dd
kyriosdata <fabio@inf.ufg.br>
2018-03-19 11:56:38 -0300

    exibe conteudo utf8

--- filesystem/exibe.js
+++ filesystem/exibe.js
@@ -8,12 +8,16 @@ if (process.argv.length != 3) {
 
 const path = process.argv[2];
 
-fs.exists(path, existe => {
-    if (existe) {
-        fs.readFile(path, "utf-8", function (e, d) {
-           console.log(d);
-        });
-    } else {
-        console.log("Arquivo não existe: " + path);
-    }
-});
+/**
+ * Exibe o conteúdo de um arquivo. Assume que se trata de um arquivo
+ * texto usando o encoding UTF-8.
+ *
+ * @param arquivo Nome do arquivo.
+ */
+function exibeArquivoUtf8(arquivo) {
+    fs.readFile(arquivo, "utf-8", function (e, d) {
+        console.log(e ? "Arquivo não existe: " + arquivo : d);
+    });
+}
+
+exibeArquivoUtf8(path);
