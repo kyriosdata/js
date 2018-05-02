c3327a476a748a956cbcd13d6a1349648a9d7d87
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-05-02 19:29:21 -0300

    documentacao parcial

--- ambientacao/classe/matematica.js
+++ ambientacao/classe/matematica.js
@@ -1,4 +1,17 @@
+/**
+ * Operações matemáticas básicas.
+ *
+ * @type {module.Matematica}
+ */
 module.exports = class Matematica {
+
+    /**
+     * Produz a soma dos argumentos fornecidos.
+     *
+     * @param {number} a Um dos argumentos.
+     * @param {number} b O outro argumento.
+     * @returns {number} A soma dos argumentos.
+     */
     soma(a, b) {
         return a + b;
     }
