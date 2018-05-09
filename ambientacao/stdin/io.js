9e9c57a4e54f879ab25a41ae0628a59c1b4538c0
Elma <elmadiniz.si@gmail.com>
2018-05-09 19:50:22 -0300

    stdin

---
+++ ambientacao/stdin/io.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Forneça uma data no formato dd/mm/aaaa? ', (data) => {
+  
+  console.log(`Data fornecida: ${data}`);
+
+  rl.close();
+});
+
+console.log("Curioso...");
