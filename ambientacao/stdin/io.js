bce0ff88c6787a7f6a0715287dcef1f99985c10c
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-05-09 21:15:04 -0300

    verifica formato de data

--- ambientacao/stdin/io.js
+++ ambientacao/stdin/io.js
@@ -5,13 +5,40 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Forneça uma data: ', (data) => {
-  
-  if (data && data.trim().lenght > 0) {
-    console.log(`Data fornecida: ${data}`);
-  } else {
-    console.log("Preciso de uma data para prosseguir...");
+function dataFromEntrada(entrada) {
+  let data = null;
+  if (entrada.match(/^(\d{2}\/\d{2}\/\d{4})$/)) {
+    try {
+      data = new Date(entrada);
+      data.toISOString();
+    } catch (e) {
+      data = null;
+    }
   }
 
-  rl.close();
-});
+  return data;
+}
+
+function processaData(data) {
+  console.log(`Data fornecida: ${data}`);
+}
+
+function pergunta() {
+  rl.question("Forneça uma data (dd/mm/aaaa): ", verificaEntrada);
+}
+
+function verificaEntrada(entrada) {  
+  const data = dataFromEntrada(entrada);
+  if (data) {
+    processaData(data);
+    rl.close();
+  } else {
+    pergunta();  
+  }
+}
+
+pergunta();
+
+
+
+
