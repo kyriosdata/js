055da2de58b16d81bb63e53967a860803a234200
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-06-06 19:45:03 -0300

    alternativa via forEach

---
+++ ambientacao/command-line/cl-foreach.js
@@ -0,0 +1,17 @@
+// Versão de linha de comandos usando "forEach"
+
+// Quando se executa o Node.js, informações sobre o
+// processo correspondente podem ser capturadas
+// por meio do objeto "process". 
+
+// Em particular, "process.argv" retorna o vetor
+// contendo os argumentos fornecidos via linha de
+// comandos quando o processo foi iniciado.
+// IMPORTANTE: as duas primeiras entradas,
+// process.argv[0] e process.argv[1], respectivamente,
+// fornecem o nome do processo (node) e o nome do 
+// arquivo JavaScript executado. Ou seja, é a partir
+// de process.argv[2] que seguem os argumentos
+// fornecidos ao programa.
+
+process.argv.forEach((e,i) => console.log(`${i} : ${e}`));
