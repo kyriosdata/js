a5adeb013e3c25385f3b79f72a594546bf2a1992
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-06-06 20:00:00 -0300

    ordena antes de exibir

--- ambientacao/environment/exibe.js
+++ ambientacao/environment/exibe.js
@@ -5,5 +5,5 @@
 // Exibe apenas as variáveis de ambiente
 // console.log(Object.keys(process.env));
 
-// Variáveis e valores correspondentes
-Object.keys(process.env).forEach(e => console.log(`${e} : ${process.env[e]}`));
+// Variáveis e valores correspondentes são exibidos, após ordenados.
+Object.keys(process.env).sort().forEach(e => console.log(`${e} : ${process.env[e]}`));
