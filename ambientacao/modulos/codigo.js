7797220121cd005ab4055c9a44d87f3cab00e754
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-06-13 20:53:25 -0300

    documentacao

--- ambientacao/modulos/codigo.js
+++ ambientacao/modulos/codigo.js
@@ -1,5 +1,8 @@
 // Exporta a função soma.
 
+/**
+ * Retorna a soma dos argumentos.
+ */
 export function soma(x, y) {
   return x + y;
 }
--- ambientacao/modulos/multiplicacao.js
+++ ambientacao/modulos/multiplicacao.js
@@ -1,4 +1,7 @@
 
+/**
+ * Retorna o produto dos argumentos.
+ */
 export function multiplicacao(x, y) {
   return x * y;
 }
