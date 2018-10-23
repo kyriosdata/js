036efffa0d83a53c0dd632fb16354577c2d728de
kyriosdata <fabio@inf.ufg.br>
2018-10-23 11:49:06 -0300

    substituicao de qunit por jest

---
+++ topicos/ambientacao/classe/test/matematica.test.js
@@ -0,0 +1,13 @@
+// Recupera a classe Matematica do módulo correspondente.
+const Matematica = require("../matematica");
+
+// Cria uma instância utilizada pelos testes
+const matematica = new Matematica();
+
+test("subtração trivial", () => {
+    expect(matematica.subtracao(100,1)).toBe(99);
+});
+
+test("soma trivial", () => {
+    expect(matematica.soma(1,2)).toBe(3);
+});
