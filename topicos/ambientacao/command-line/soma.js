b71432daa361ee7ffb0d73ec55fb773ba161ad2a
kyriosdata <fabio@inf.ufg.br>
2018-10-23 14:12:49 -0300

    documentacao

--- topicos/ambientacao/command-line/soma.js
+++ topicos/ambientacao/command-line/soma.js
@@ -1,7 +1,13 @@
 /**
  * Obtém parcelas (argumentos de entrada).
  */
-function obtemParcelas() {
+function recuperaDoisArgumentosLinhaDeComandos() {
+
+    // Observe que dois primeiros argumentos são o node e a própria aplicação
+    if (process.argv.length != 4) {
+        throw Error("uso correto exige dois argumentos");
+    }
+
     const parcela1 = parseInt(process.argv[2]);
     const parcela2 = parseInt(process.argv[3]);
 
@@ -12,9 +18,9 @@ function obtemParcelas() {
  * Apenas exibe soma dos argumentos fornecidos.
  */
 function programa() {
-    const [ p1, p2 ] = obtemParcelas();
+    const [ p1, p2 ] = recuperaDoisArgumentosLinhaDeComandos();
 
-    console.log(p1 + p2);
+    console.log(`Soma dos argumentos é ${p1 + p2}`);
 }
 
 programa();
