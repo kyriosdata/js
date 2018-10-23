c0f6e0eb8ebb31200d03f15d1dddf36ccbfc0776
kyriosdata <fabio@inf.ufg.br>
2018-10-23 14:44:53 -0300

    cobertura de testes

--- topicos/ambientacao/command-line/test/recuperaArgumentos.test.js
+++ topicos/ambientacao/command-line/test/recuperaArgumentos.test.js
@@ -11,4 +11,25 @@ test("nao definido para menos de dois argumentos", () => {
 
 test("nao definido para mais de 2 argumentos", () => {
     expect(() => { argumentos.recupera([1,2,3]);}).toThrow();
-})
+});
+
+test("recuperacao correta dos dois argumentos", () => {
+    const recuperado = argumentos.recupera([ 1, 2 ]);
+
+    expect(recuperado.length).toBe(2);
+});
+
+test("quando nao é process.argv apenas 2 argumentos", () => {
+
+    expect(() => { argumentos.recupera([1, 2, 3, 4]); }).toThrow();
+});
+
+test("process.argv tratado diferentemente", () => {
+    process.argv = [ 1, 2, 3, 4];
+
+    const recuperado = argumentos.recupera(process.argv);
+
+    expect(recuperado).toHaveLength(2);
+    expect(recuperado[0]).toBe(3);
+    expect(recuperado[1]).toBe(4);
+});
