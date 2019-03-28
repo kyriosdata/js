767ece8968dd5c139620417613fc145dfe2cdcc4
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-03-28 08:49:27 -0300

    melhorias

--- topicos/inicio/teste/test/codigo.test.js
+++ topicos/inicio/teste/test/codigo.test.js
@@ -1,3 +1,13 @@
+// No final do arquivo codigo.js encontra-se
+// exports.soma = soma;
+// 
+// Conforme comentado no próprio arquivo codigo.js,
+// module.exports é a referência para o objeto retornado
+// por chamadas require(). Ou seja, abaixo, o que é
+// "operacao" será o mesmo que "exports", ou seja,
+// "operacao.soma" é a função "soma" definida no
+// arquivo codigo.js.
+
 const operacao = require("../codigo");
 
 test("1+1=2", () => {
