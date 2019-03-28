3298a8a17dcc8e64eb9f7f8c23fbd9bc5748ff25
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-03-28 20:16:50 -0300

    exercicios

---
+++ exercicios/iteracao/vetor/menor-temperatura.js
@@ -0,0 +1,17 @@
+/**
+ * Identifica a menor temperatura presente dentre os valores fornecidos.
+ * 
+ * @param {number[]} vetor Vetor de temperaturas (números).
+ * 
+ * @returns O menor valor presente no vetor fornecido.
+ * 
+ * @throws {TypeError} Se o argumento fornecido não for um vetor.
+ * Este objeto também é gerado se a função não é chamada exatamente
+ * com um único argumento.
+ * 
+ * @throws {RangeError} Se o vetor fornecido estiver vazio.
+ */
+function menorTemperatura(vetor) {
+}
+
+module.exports = menorTemperatura;
