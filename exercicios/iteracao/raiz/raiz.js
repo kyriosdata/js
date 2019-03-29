9e55bea3ad0eabcf4412d4cce80382b9bf22e815
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-03-29 09:51:58 -0300

    raiz

--- exercicios/iteracao/raiz/raiz.js
+++ exercicios/iteracao/raiz/raiz.js
@@ -1,11 +1,14 @@
 function raiz(n, delta) {
+    let contador = 0;
     let i = 1;
     let f = n;
     while (true) {
+        contador = contador + 1;
         let r = (f - i) / 2 + i;
         let quadrado = r * r;
 
         if (Math.abs(quadrado - n) < delta) {
+            console.log(`Iteracoes realizadas: ${contador}`);
             return r;
         }
 
--- exercicios/iteracao/raiz/test/raiz.test.js
+++ exercicios/iteracao/raiz/test/raiz.test.js
@@ -1,5 +1,7 @@
 const raiz = require("../raiz");
 
-test("caso simples",() => {
+test("casos simples",() => {
     expect(raiz(4, 0.001)).toBeCloseTo(2);
+    expect(raiz(16, 0.001)).toBeCloseTo(4);
+    expect(raiz(100, 0.001)).toBeCloseTo(10);
 });
