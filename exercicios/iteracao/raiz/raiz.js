c4453945949f4caba9721a5fb46eafe11cc11732
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-03-29 10:22:16 -0300

    raiz

--- exercicios/iteracao/raiz/raiz.js
+++ exercicios/iteracao/raiz/raiz.js
@@ -1,14 +1,29 @@
-function raiz(n, delta) {
-    let contador = 0;
+/**
+ * Extrai a raiz quadrada do número fornecido com a precisão
+ * indicada. Um  método para extração da raiz quadrada baseado
+ * em aproximações (talvez seja variante do método de Newton). 
+ * 
+ * Não há intenção em reinventar a roda. Aqui é apenas um 
+ * exercício.
+ * 
+ * @param {number} n Número cuja raiz é desejada.
+ * @param {number} precisao Precisão esperada para o cálculo da raiz.
+ * 
+ * @returns {number} A raiz quadrada do número fornecido com 
+ * a precisão indicada.
+ * 
+ * @todo Tratar casos excepcionais e documentar (p.ex., valores negativos)
+ * @todo Otimizar a atribuição ao valor r (simplificar a expressao, eliminar subtracao)
+ * @todo A chamada ao método abs é desnecessária, calcular limites previamente. 
+ */
+function raiz(n, precisao) {
     let i = 1;
     let f = n;
     while (true) {
-        contador = contador + 1;
         let r = (f - i) / 2 + i;
         let quadrado = r * r;
 
-        if (Math.abs(quadrado - n) < delta) {
-            console.log(`Iteracoes realizadas: ${contador}`);
+        if (Math.abs(quadrado - n) < precisao) {
             return r;
         }
 
--- exercicios/iteracao/raiz/test/raiz.test.js
+++ exercicios/iteracao/raiz/test/raiz.test.js
@@ -1,7 +1,12 @@
 const raiz = require("../raiz");
 
-test("casos simples",() => {
+test("raiz de quadrados",() => {
     expect(raiz(4, 0.001)).toBeCloseTo(2);
     expect(raiz(16, 0.001)).toBeCloseTo(4);
     expect(raiz(100, 0.001)).toBeCloseTo(10);
+    expect(raiz(10000, 0.001)).toBeCloseTo(100);
+});
+
+test("raiz de números não inteiros", () => {
+    expect(raiz(2, 0.001)).toBeCloseTo(1.414);
 });
