100455e838cf907b675358ecc15162e40cbdaf32
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-03-29 10:47:12 -0300

    somar impares

--- exercicios/iteracao/vetor/somar-impares.js
+++ exercicios/iteracao/vetor/somar-impares.js
@@ -1,3 +1,17 @@
+/**
+ * Identifica o total (soma) dos valores ímpares presentes
+ * no vetor fornecido.
+ * 
+ * @param {number[]} vetor Vetor de valores inteiros
+ * @returns A soma dos elementos do vetor que são ímpares.
+ * 
+ * @throws {TypeError} Gerado em vários cenários: (a) se o argumento 
+ * fornecido não for um vetor; (b) se a função não é chamada exatamente
+ * com um único argumento; (c) se o vetor não contém apenas valores
+ * numéricos e (d) se alguém valor númerico não é inteiro.
+ * 
+ */
 function somarImpares(vetor) {
-    
 }
+
+module.exports = somarImpares;
