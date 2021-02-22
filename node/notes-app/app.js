882bb9bf6b4862e0644c059f2e7d1c1781a21f91
kyriosdata <fabio@inf.ufg.br>
2021-02-22 14:32:50 -0300

    modulos

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,5 +1,8 @@
 const notes = require("./notes");
+var validator = require("validator");
 
 const notas = notes.getNotes();
 
+console.log("NOTAS APP");
 console.log(notas);
+console.log(validator.isEmail("foo@bar.com"));
