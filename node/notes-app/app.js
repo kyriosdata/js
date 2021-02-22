5024254cfff085024ca3ac91209d05fa51a4a174
kyriosdata <fabio@inf.ufg.br>
2021-02-22 14:55:51 -0300

    nodemon

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -4,8 +4,6 @@ const notes = require("./notes");
 
 const notas = notes.getNotes();
 
-console.log("NOTAS APP");
+console.log("MINHAS NOTAS APP");
 console.log(notas);
-console.log(validator.isEmail("123@ufg.br"));
 console.log(chalk.green("sucesso!"));
-console.log(chalk.bgGreen("sucesso"));
