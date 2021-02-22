4ed904a1d7b7797fbb37bd76ec7ea92892e9c587
kyriosdata <fabio@inf.ufg.br>
2021-02-22 19:01:06 -0300

    yargs

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,9 +1,39 @@
 const chalk = require("chalk");
 const validator = require("validator");
+const yargs = require("yargs");
 const notes = require("./notes");
 
-const notas = notes.getNotes();
+// Configura a versão
+yargs.version("1.23.0-beta");
 
+// Comando add
+yargs.command({
+  command: "add",
+  describe: "Adiciona uma nota",
+  handler: function () {
+    console.log("adding a new note");
+  },
+});
+
+// Comando list
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: () => {
+    console.log("list of my notes..");
+  },
+});
+
+// Comando read
+yargs.command({
+  command: "read",
+  describe: "read a specific note",
+  handler: () => console.log("show the specific note"),
+});
+
+// Título da aplicação
 console.log("MINHAS NOTAS APP");
-console.log(notas);
-console.log(chalk.green("sucesso!"));
+
+const notas = notes.getNotes();
+const argv = yargs.argv;
+//console.log(yargs.argv);
