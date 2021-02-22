d5fe81bfe561086b4adf100e4333d0f35765923b
kyriosdata <fabio@inf.ufg.br>
2021-02-22 14:41:27 -0300

    notes

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,8 +1,11 @@
+const chalk = require("chalk");
+const validator = require("validator");
 const notes = require("./notes");
-var validator = require("validator");
 
 const notas = notes.getNotes();
 
 console.log("NOTAS APP");
 console.log(notas);
-console.log(validator.isEmail("foo@bar.com"));
+console.log(validator.isEmail("123@ufg.br"));
+console.log(chalk.green("sucesso!"));
+console.log(chalk.bgGreen("sucesso"));
