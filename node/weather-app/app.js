c7be349e469b42248133ec39cbdd7ad362e00b6d
kyriosdata <fabio@inf.ufg.br>
2021-02-26 01:08:25 -0300

    weather

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -4,23 +4,28 @@ const host = "http://api.weatherstack.com/current?";
 const getParams = (key, local) => `access_key=${key}&query=${local}`;
 const url = host + getParams("a6b608ea77ab5a41c318022931d84b9d", "-16,-49");
 
-function resposta(res) {
-  const chunks = [];
+function getTemperatura(callback) {
+  function internaResposta(res) {
+    const chunks = [];
 
-  res.on("data", (chunk) => chunks.push(chunk));
+    res.on("data", (chunk) => chunks.push(chunk));
 
-  res.on("end", function (chunk) {
-    const body = Buffer.concat(chunks);
-    const json = body.toString();
-    const objeto = JSON.parse(json);
+    res.on("end", function (chunk) {
+      const body = Buffer.concat(chunks);
+      const json = body.toString();
+      const objeto = JSON.parse(json);
 
-    if (objeto.error) {
-      console.log("houve um erro...");
-    } else {
-      console.log("Temperatura:", objeto.current.temperature);
-    }
-  });
+      if (objeto.error) {
+        console.log("houve um erro...");
+      } else {
+        callback(
+          `It is ${objeto.current.temperature}, but seems ${objeto.current.feelslike}`
+        );
+      }
+    });
+  }
+
+  http.get(url, internaResposta).end();
 }
 
-const request = http.get(url, resposta);
-request.end();
+getTemperatura(console.log);
