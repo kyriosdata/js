af68766ac6ad16a8eb4604a5fc7ce4f4dda2b4de
kyriosdata <fabio@inf.ufg.br>
2021-02-26 00:07:51 -0300

    weather

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,10 +1,26 @@
-console.log("starting");
+const http = require("follow-redirects").http;
 
-const exibeInterrogacao = () => console.log("?");
-const exibeApos3s = () => console.log("apos 3s");
+const host = "http://api.weatherstack.com/current?";
+const getParams = (key, local) => `access_key=${key}&query=${local}`;
+const url = host + getParams("a6b608ea77ab5a41c318022931d84b9d", "-16,-49");
 
-setTimeout(exibeInterrogacao, 0);
+function resposta(res) {
+  const chunks = [];
 
-setTimeout(exibeApos3s, 3000);
+  res.on("data", (chunk) => chunks.push(chunk));
 
-console.log("stoping");
+  res.on("end", function (chunk) {
+    const body = Buffer.concat(chunks);
+    const json = body.toString();
+    const objeto = JSON.parse(json);
+
+    if (objeto.error) {
+      console.log("houve um erro...");
+    } else {
+      console.log("Temperatura:", objeto.current.temperature);
+    }
+  });
+}
+
+const request = http.get(url, resposta);
+request.end();
