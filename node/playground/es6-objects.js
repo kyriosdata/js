5914573dbfe1c3bdba0726c96c75be346973a6bf
kyriosdata <fabio@inf.ufg.br>
2021-03-01 19:40:26 -0300

    organizacao

---
+++ node/playground/es6-objects.js
@@ -0,0 +1,13 @@
+const meuNome = "F";
+
+const user = {
+  nome: meuNome,
+  age: 23,
+  location: "São Paulo",
+};
+
+console.log(user);
+
+const { nome, age, location } = user;
+
+console.log(nome);
