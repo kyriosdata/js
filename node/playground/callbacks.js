242bf03e1d65e5ba77bae64ae802bec87bcf913f
kyriosdata <fabio@inf.ufg.br>
2021-03-08 17:46:21 -0300

    operacoes

--- node/playground/callbacks.js
+++ node/playground/callbacks.js
@@ -1,3 +1,17 @@
 const add = (x, y, c) => setTimeout(() => c(x + y), 2000);
 
-add(1, 4, console.log);
+// add(1, 4, console.log);
+
+function algumaOperacaoAssincrona(f) {
+  setTimeout(() => f(undefined, "funcionou"), 2000);
+}
+
+function callbackToExecutAtTheEnd(error, result) {
+  if (error) {
+    console.log("erro:", error);
+  } else {
+    console.log("ok", result);
+  }
+}
+
+algumaOperacaoAssincrona(callbackToExecutAtTheEnd);
