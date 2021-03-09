e84560b95b2888984999aade70b1f8a1b48c36f7
kyriosdata <fabio@inf.ufg.br>
2021-03-08 22:14:31 -0300

    mongoose

--- node/mongodb-client/src/db/mongoose.js
+++ node/mongodb-client/src/db/mongoose.js
@@ -1,4 +1,21 @@
 const mongoose = require("mongoose");
-const conexaoUrl = require("./mongodb-utils");
+const { conexaoUrl } = require("./mongodb-utils");
 
-mongoose;
+mongoose.connect(conexaoUrl, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({ name: "Fábio", age: 53 });
+
+me.save().then(console.log).catch(console.log);
