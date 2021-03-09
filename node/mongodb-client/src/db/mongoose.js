4a092493b6bc69d98fa21cb7200de70228d40d14
kyriosdata <fabio@inf.ufg.br>
2021-03-09 09:32:16 -0300

    mongoose

--- node/mongodb-client/src/db/mongoose.js
+++ node/mongodb-client/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const { conexaoUrl } = require("./mongodb-utils");
 
 mongoose.connect(conexaoUrl, {
@@ -8,14 +9,51 @@ mongoose.connect(conexaoUrl, {
 });
 
 const User = mongoose.model("User", {
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(valor) {
+      if (valor.toLowerCase().includes("password")) {
+        throw new Error("Invalid term in password");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(valor) {
+      if (!validator.isEmail(valor)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
   description: {
     type: String,
   },
   completed: {
     type: Boolean,
+    default: false,
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate(valor) {
+      if (valor < 0 || valor > 200) {
+        throw new Error("idade invalida " + valor);
+      }
+    },
   },
 });
 
-const me = new User({ description: "Fábio", completed: true });
+const me = new User({
+  description: "Fábio",
+  email: " TESTE@ufg.BR ",
+  age: 22,
+  password: "  apas232323   ",
+});
 
 me.save().then(console.log).catch(console.log);
