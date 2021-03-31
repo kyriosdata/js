6092b10432c9f3e79346570a9d2b2429cfa1ca56
kyriosdata <fabio@inf.ufg.br>
2021-03-31 10:58:21 -0300

    conexao com MongoDB nao e fechada (erro)

--- node/task-manager/tests/status.test.js
+++ node/task-manager/tests/status.test.js
@@ -1,11 +1,6 @@
 const app = require("../src/app");
 const request = require("supertest");
 
-test("status", () => {
-  request(app)
-    .get("")
-    .expect(200)
-    .end(function (err, res) {
-      if (err) throw err;
-    });
+test("status", async () => {
+  await request(app).get("").expect(200);
 });
