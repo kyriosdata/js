30215fe56a3fcd192c9267c269d377c91c1c756b
kyriosdata <fabio@inf.ufg.br>
2022-01-08 20:30:20 -0300

    noções (threads em Nodejs)

--- node/basico/calculo.js
+++ node/basico/calculo.js
@@ -1,3 +1,11 @@
-const { isMainThread } = require("worker_threads");
+const { isMainThread, parentPort, workerData } = require("worker_threads");
 
-console.log("calculo.js isMainThread", isMainThread);
+console.log("filho isMainThread", isMainThread);
+console.log("Mensagem recebida quando nasci: ", workerData);
+
+parentPort.postMessage("bença");
+parentPort.postMessage("1");
+parentPort.postMessage("2");
+parentPort.postMessage("3");
+
+process.on("exit", () => console.log("filho saindo..."));
--- node/basico/worker-exemplo.js
+++ node/basico/worker-exemplo.js
@@ -1,5 +1,12 @@
 const { Worker, isMainThread } = require("worker_threads");
-const worker = new Worker("./calculo.js");
+
+const worker = new Worker("./calculo.js", { workerData: "Seja bem-vindo!" });
+
 worker.postMessage("calcule");
 
-console.log("worker-example.js isMainThread", isMainThread);
+console.log("pai isMainThread", isMainThread);
+
+worker.on("message", (data) => console.log("filho requisita: ", data));
+worker.on("exit", console.log);
+
+process.on("exit", () => console.log("pai saindo..."));
